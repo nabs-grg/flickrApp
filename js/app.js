057a2c90f8bdd39ef3b936985edcb5fe2522151f
Narbu Gurung <ngurung@spartaglobal.co>
2016-11-09 16:58:46 +0000

    refractor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){ //once everything is loaded in the page start executing jQuery code
 
+
 	var flickr_api = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 	var req = {
 		tags : "wembley",
@@ -10,15 +11,15 @@ $(document).ready(function(){ //once everything is loaded in the page start exec
 		console.log("success");
 		console.log(data);
 
-		var db = $(document.body);
-		db.append('<h3>' + data.title + '</h3>');
+		var flickrAdd = $(document.body);
+		flickrAdd.append('<h3>' + data.title + '</h3>');
 
 		console.log(data.title);
 
 		$.each(data.items,function(index,value){
 			console.log("=============================================================================");
-			db.append("<br />");
-			db.append('<img src="' + value.media.m + '" >' + "<br />");
+			flickrAdd.append("<br />");
+			flickrAdd.append('<img src="' + value.media.m + '" >' + "<br />");
 
 		});
 	});
