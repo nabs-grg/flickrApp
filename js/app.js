65b563c2bd944359145de0e4b22d69d3671fa09c
Narbu Gurung <ngurung@spartaglobal.co>
2016-11-09 21:42:40 +0000

    search funtionality added with the tag for flickr app

--- js/app.js
+++ js/app.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){ //once everything is loaded in the page start executing jQuery code
 
+	
+	$('#flickr-search').click(function(){
 
-	var flickr_api = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-	var req = {
-		tags : "wembley",
-		format : "json"
-	};
+		var flickrTag = $('#flickr-tag').val();
+		console.log(flickrTag);
 
-	$.getJSON(flickr_api,req,function(data){
-		console.log("success");
-		console.log(data);
 
-		var flickrAdd = $(document.body);
-		flickrAdd.append('<h3>' + data.title + '</h3>');
+		var flickr_api = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
-		console.log(data.title);
+		var req = {
+			
+			tags : flickrTag, 
+			format : "json"
 
-		$.each(data.items,function(index,value){
-			console.log("=============================================================================");
-			flickrAdd.append("<br />");
-			flickrAdd.append('<img src="' + value.media.m + '" >' + "<br />");
+		};
 
+		$.getJSON(flickr_api,req,function(data){
+			console.log("success");
+			console.log(data);
+
+			var flickrAdd = $(document.body);
+			$('#tag-info').html(data.title);
+
+			console.log(data.title);
+
+			$.each(data.items,function(index,value){
+				
+				flickrAdd.append("<br />");
+				flickrAdd.append('<img src="' + value.media.m + '" >' + "<br />");
+
+			});
 		});
+
+		$('#flickr-tag').val('');
+
 	});
 
 });	
