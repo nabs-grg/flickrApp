d2161d58d77817d80c88ebcd92d33cdad7a0c8ee
Narbu Gurung <ngurung@spartaglobal.co>
2016-11-09 16:57:34 +0000

    added sass file and made a request to flickr API to render the image interms of tags

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
-$(function(){
+$(document).ready(function(){ //once everything is loaded in the page start executing jQuery code
 
-	$("#square").click(function(){
-		alert("hello world");
+	var flickr_api = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var req = {
+		tags : "wembley",
+		format : "json"
+	};
+
+	$.getJSON(flickr_api,req,function(data){
+		console.log("success");
+		console.log(data);
+
+		var db = $(document.body);
+		db.append('<h3>' + data.title + '</h3>');
+
+		console.log(data.title);
+
+		$.each(data.items,function(index,value){
+			console.log("=============================================================================");
+			db.append("<br />");
+			db.append('<img src="' + value.media.m + '" >' + "<br />");
+
+		});
 	});
 
-});
+});	
+
+
+
